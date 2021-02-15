863d29bb7301783d9b7f013eb579224adb009283
Jack Sanders <jacklewissanders@gmail.com>
2021-02-15 11:18:04 -0800

    add xcode project

---
+++ PetTinder/PetTinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PetTinder
+//
+//  Created by Jack Sanders on 2/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PetTinder/PetTinder/PetTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTinderApp.swift
+//  PetTinder
+//
+//  Created by Jack Sanders on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct PetTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
