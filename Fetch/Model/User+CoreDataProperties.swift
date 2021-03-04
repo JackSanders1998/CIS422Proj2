fd99552f54cce87a135ee64112b527690b433712
westcallista <77295181+westcallista@users.noreply.github.com>
2021-03-04 08:05:43 -0800

    Update User+CoreDataProperties.swift

--- Fetch/Model/User+CoreDataProperties.swift
+++ Fetch/Model/User+CoreDataProperties.swift
@@ -11,17 +11,37 @@ import CoreData
 
 
 public class User: NSManagedObject, Identifiable {
-    @NSManaged public var breed: String?
-    @NSManaged public var name: String?
-    @NSManaged public var dogName: String?
+    @NSManaged public var breed: String
+    @NSManaged public var name: String
+    @NSManaged public var dogName: String
     @NSManaged public var created: Date?
-    
+    @NSManaged public var city: String
 
 }
 
 extension User {
     static func getAllUsers() -> NSFetchRequest<User> {
         let request: NSFetchRequest<User> = User.fetchRequest() as! NSFetchRequest<User>
+        
+//        let sortDescriptor = NSSortDescriptor(key: "created", ascending: true)
+//
+//        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
+
+public class CurrentUser: NSManagedObject, Identifiable {
+    @NSManaged public var name: String
+    @NSManaged public var created: Date
+    @NSManaged public var dogbreed: String
+    @NSManaged public var dogname: String
+    
+}
+
+extension CurrentUser {
+    static func getAllUsers() -> NSFetchRequest<CurrentUser> {
+        let request: NSFetchRequest<CurrentUser> = CurrentUser.fetchRequest() as! NSFetchRequest<CurrentUser>
         
 //        let sortDescriptor = NSSortDescriptor(key: "created", ascending: true)
 //
