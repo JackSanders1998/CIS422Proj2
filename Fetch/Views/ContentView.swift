9d79b3560982735b3428c6758a2b59e2dbaab6aa
Ronny Fuentes <Ronnyf@uoregon.edu>
2021-03-09 14:02:56 -0800

    Bug fix
    Should be a clean version to test database

--- Fetch/Views/ContentView.swift
+++ Fetch/Views/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var session: SessionStore
+    
     func getUser() {
         session.listen()
     }
@@ -17,8 +18,11 @@ struct ContentView: View {
         Group {
             if (session.session != nil) {
                 Text("Welcome back!")
+                Button(action: session.signOut) {
+                    Text("Sign out")
+                }
             } else {
-                Text("Show auth screen")
+                AuthenticationView()
             }
         }.onAppear(perform: getUser)
     }
